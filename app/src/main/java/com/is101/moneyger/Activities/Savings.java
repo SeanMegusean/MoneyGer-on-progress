f76ef99d58d4cfd05b31dd9da6adbda65650aeac
Sean <fernandez.sean.marino@gmail.com>
2024-10-19 02:01:54 +0800

    gagu

--- app/src/main/java/com/is101/moneyger/Activities/Savings.java
+++ app/src/main/java/com/is101/moneyger/Activities/Savings.java
@@ -17,12 +17,12 @@ import com.is101.moneyger.R;
  */
 public class Savings extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
@@ -38,7 +38,6 @@ public class Savings extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment Savings.
      */
-    // TODO: Rename and change types and number of parameters
     public static Savings newInstance(String param1, String param2) {
         Savings fragment = new Savings();
         Bundle args = new Bundle();
